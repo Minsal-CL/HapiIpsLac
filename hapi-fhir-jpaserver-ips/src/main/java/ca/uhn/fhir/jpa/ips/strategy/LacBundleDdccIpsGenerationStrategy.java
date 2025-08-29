0bda080b2be11fb00eb726227412e3399f6887fe
Franco <francoulloamedina@gmail.com>
2025-08-29 08:52:49 -0400

    cambio del IPS-ddcc al IPS-ICVP

--- hapi-fhir-jpaserver-ips/src/main/java/ca/uhn/fhir/jpa/ips/strategy/LacBundleDdccIpsGenerationStrategy.java
+++ hapi-fhir-jpaserver-ips/src/main/java/ca/uhn/fhir/jpa/ips/strategy/LacBundleDdccIpsGenerationStrategy.java
@@ -36,12 +36,12 @@ public class LacBundleDdccIpsGenerationStrategy extends DefaultJpaIpsGenerationS
 	@Override
 	public String getBundleProfile() {
 		System.out.println("Lac strategy profile requested!");
-		return "http://lacpass.racsel.org/StructureDefinition/lac-bundle-ddcc";
+		return "http://smart.who.int/icvp/StructureDefinition/Bundle-uv-ips-ICVP";
 	}
 
     @Override
 	public String getCompositionProfile() {
-		return "http://lacpass.racsel.org/StructureDefinition/lac-composition-ddcc";
+		return "http://smart.who.int/icvp/StructureDefinition/Composition-uv-ips-ICVP";
 	}
 
 	/**
@@ -49,81 +49,91 @@ public class LacBundleDdccIpsGenerationStrategy extends DefaultJpaIpsGenerationS
 	 */
 	@Override
 	protected void addSections() {
-		addJpaSectionAllergyIntoleranceNoInfoOnly();
-		addJpaSectionMedicationSummaryNoInfoOnly();
-		addJpaSectionProblemListNoInfoOnly();
-        addJpaSectionImmunizationsNoInfo();	
+		addJpaSectionAllergyIntolerance();
+		addJpaSectionMedicationSummary();
+		addJpaSectionProblemList();
+		addJpaSectionImmunizationsNoInfo();
+		addJpaSectionProcedures();
+		addJpaSectionMedicalDevices();
+		addJpaSectionDiagnosticResults();
+		addJpaSectionVitalSigns();
+		addJpaSectionPregnancy();
+		addJpaSectionSocialHistory();
+		addJpaSectionIllnessHistory();
+		addJpaSectionFunctionalStatus();
+		addJpaSectionPlanOfCare();
+		addJpaSectionAdvanceDirectives();
 	}
 
 	/**
 	 * Sección de inmunizaciones con NoInfoGenerator.
 	 */
 	
-	protected void addJpaSectionAllergyIntoleranceNoInfoOnly() {
-		Section section = Section.newBuilder()
-			.withTitle("Alergías e Intolerancias")
-			.withSectionSystem(SECTION_SYSTEM_LOINC)
-			.withSectionCode(SECTION_CODE_ALLERGY_INTOLERANCE)
-			.withSectionDisplay("Allergies and adverse reactions Document")
-			.withResourceType(org.hl7.fhir.r4.model.AllergyIntolerance.class)
-			.withProfile("https://hl7.org/fhir/uv/ips/StructureDefinition-Composition-uv-ips-definitions.html#Composition.section:sectionAllergies")
-			.withNoInfoGenerator(new AllergyIntoleranceNoInfoR4Generator())
-			.build();
+	// protected void addJpaSectionAllergyIntoleranceNoInfoOnly() {
+	// 	Section section = Section.newBuilder()
+	// 		.withTitle("Alergías e Intolerancias")
+	// 		.withSectionSystem(SECTION_SYSTEM_LOINC)
+	// 		.withSectionCode(SECTION_CODE_ALLERGY_INTOLERANCE)
+	// 		.withSectionDisplay("Allergies and adverse reactions Document")
+	// 		.withResourceType(org.hl7.fhir.r4.model.AllergyIntolerance.class)
+	// 		.withProfile("https://hl7.org/fhir/uv/ips/StructureDefinition-Composition-uv-ips-definitions.html#Composition.section:sectionAllergies")
+	// 		.withNoInfoGenerator(new AllergyIntoleranceNoInfoR4Generator())
+	// 		.build();
 
-		JpaSectionSearchStrategyCollection strategies = JpaSectionSearchStrategyCollection.newBuilder()
-			.addStrategy(org.hl7.fhir.r4.model.AllergyIntolerance.class,
-						new ca.uhn.fhir.jpa.ips.jpa.section.EmptyAllergyIntoleranceSearchStrategy())
-			.build();
+	// 	JpaSectionSearchStrategyCollection strategies = JpaSectionSearchStrategyCollection.newBuilder()
+	// 		.addStrategy(org.hl7.fhir.r4.model.AllergyIntolerance.class,
+	// 					new ca.uhn.fhir.jpa.ips.jpa.section.EmptyAllergyIntoleranceSearchStrategy())
+	// 		.build();
 
-		addJpaSection(section, strategies);
-	}
+	// 	addJpaSection(section, strategies);
+	// }
 
-	protected void addJpaSectionMedicationSummaryNoInfoOnly() {
-		Section section = Section.newBuilder()
-			.withTitle("Lista de Medicamentos")
-			.withSectionSystem(SECTION_SYSTEM_LOINC)
-			.withSectionCode(SECTION_CODE_MEDICATION_SUMMARY)
-			.withSectionDisplay("History of Medication use Narrative")
-			.withResourceType(org.hl7.fhir.r4.model.MedicationStatement.class)
-			.withResourceType(org.hl7.fhir.r4.model.MedicationRequest.class)
-			.withResourceType(org.hl7.fhir.r4.model.MedicationAdministration.class)
-			.withResourceType(org.hl7.fhir.r4.model.MedicationDispense.class)
-			.withProfile("https://hl7.org/fhir/uv/ips/StructureDefinition-Composition-uv-ips-definitions.html#Composition.section:sectionMedications")
-			.withNoInfoGenerator(new MedicationNoInfoR4Generator())
-			.build();
+	// protected void addJpaSectionMedicationSummaryNoInfoOnly() {
+	// 	Section section = Section.newBuilder()
+	// 		.withTitle("Lista de Medicamentos")
+	// 		.withSectionSystem(SECTION_SYSTEM_LOINC)
+	// 		.withSectionCode(SECTION_CODE_MEDICATION_SUMMARY)
+	// 		.withSectionDisplay("History of Medication use Narrative")
+	// 		.withResourceType(org.hl7.fhir.r4.model.MedicationStatement.class)
+	// 		.withResourceType(org.hl7.fhir.r4.model.MedicationRequest.class)
+	// 		.withResourceType(org.hl7.fhir.r4.model.MedicationAdministration.class)
+	// 		.withResourceType(org.hl7.fhir.r4.model.MedicationDispense.class)
+	// 		.withProfile("https://hl7.org/fhir/uv/ips/StructureDefinition-Composition-uv-ips-definitions.html#Composition.section:sectionMedications")
+	// 		.withNoInfoGenerator(new MedicationNoInfoR4Generator())
+	// 		.build();
 
-		JpaSectionSearchStrategyCollection strategies = JpaSectionSearchStrategyCollection.newBuilder()
-			.addStrategy(org.hl7.fhir.r4.model.MedicationAdministration.class,
-						new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationAdministrationSearchStrategy())
-			.addStrategy(org.hl7.fhir.r4.model.MedicationDispense.class,
-						new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationDispenseSearchStrategy())
-			.addStrategy(org.hl7.fhir.r4.model.MedicationRequest.class,
-						new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationRequestSearchStrategy())
-			.addStrategy(org.hl7.fhir.r4.model.MedicationStatement.class,
-						new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationStatementSearchStrategy())
-			.build();
+	// 	JpaSectionSearchStrategyCollection strategies = JpaSectionSearchStrategyCollection.newBuilder()
+	// 		.addStrategy(org.hl7.fhir.r4.model.MedicationAdministration.class,
+	// 					new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationAdministrationSearchStrategy())
+	// 		.addStrategy(org.hl7.fhir.r4.model.MedicationDispense.class,
+	// 					new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationDispenseSearchStrategy())
+	// 		.addStrategy(org.hl7.fhir.r4.model.MedicationRequest.class,
+	// 					new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationRequestSearchStrategy())
+	// 		.addStrategy(org.hl7.fhir.r4.model.MedicationStatement.class,
+	// 					new ca.uhn.fhir.jpa.ips.jpa.section.EmptyMedicationStatementSearchStrategy())
+	// 		.build();
 
-		addJpaSection(section, strategies);
-	}
+	// 	addJpaSection(section, strategies);
+	// }
 
-	protected void addJpaSectionProblemListNoInfoOnly() {
-		Section section = Section.newBuilder()
-			.withTitle("Lista de Problemas")
-			.withSectionSystem(SECTION_SYSTEM_LOINC)
-			.withSectionCode(SECTION_CODE_PROBLEM_LIST)
-			.withSectionDisplay("Problem list - Reported")
-			.withResourceType(org.hl7.fhir.r4.model.Condition.class)
-			.withProfile("https://hl7.org/fhir/uv/ips/StructureDefinition-Composition-uv-ips-definitions.html#Composition.section:sectionProblems")
-			.withNoInfoGenerator(new ProblemNoInfoR4Generator())
-			.build();
+	// protected void addJpaSectionProblemListNoInfoOnly() {
+	// 	Section section = Section.newBuilder()
+	// 		.withTitle("Lista de Problemas")
+	// 		.withSectionSystem(SECTION_SYSTEM_LOINC)
+	// 		.withSectionCode(SECTION_CODE_PROBLEM_LIST)
+	// 		.withSectionDisplay("Problem list - Reported")
+	// 		.withResourceType(org.hl7.fhir.r4.model.Condition.class)
+	// 		.withProfile("https://hl7.org/fhir/uv/ips/StructureDefinition-Composition-uv-ips-definitions.html#Composition.section:sectionProblems")
+	// 		.withNoInfoGenerator(new ProblemNoInfoR4Generator())
+	// 		.build();
 
-		JpaSectionSearchStrategyCollection strategies = JpaSectionSearchStrategyCollection.newBuilder()
-			.addStrategy(org.hl7.fhir.r4.model.Condition.class,
-				new ca.uhn.fhir.jpa.ips.jpa.section.EmptyConditionSearchStrategy())
-			.build();
+	// 	JpaSectionSearchStrategyCollection strategies = JpaSectionSearchStrategyCollection.newBuilder()
+	// 		.addStrategy(org.hl7.fhir.r4.model.Condition.class,
+	// 			new ca.uhn.fhir.jpa.ips.jpa.section.EmptyConditionSearchStrategy())
+	// 		.build();
 
-		addJpaSection(section, strategies);
-	}
+	// 	addJpaSection(section, strategies);
+	// }
 
 	protected void addJpaSectionImmunizationsNoInfo() {
 		Section section = Section.newBuilder()
